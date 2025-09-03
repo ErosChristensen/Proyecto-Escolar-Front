2411c1bd4b147bace2b7ecd5e7f3a4ff3b9e0fc4
JuanaDegreef <juanaamdzs@gmail.com>
2025-09-03 10:49:44 -0300

    Carpeta Components y Styles creadas

---
+++ src/components/Noticias_Inicio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Noticias_Inicio() {
+  return (
+    <div className="">
+      <p className="">Hola desde componente</p>
+    </div>
+  );
+}
+
+export default Noticias_Inicio;
