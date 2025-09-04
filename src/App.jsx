9af277600e3c7eb8ce18f9d7cebb9e5fba210dc6
ErosChristensen <erosbarker@gmail.com>
2025-09-04 08:43:58 -1200

    vinculado noticias+footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Noticias_Inicio from './components/Noticias_Inicio'
 import './App.css'
 import Footer from './components/Footer'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <div>
+      <Noticias_Inicio />
       <Footer />
     </div>
     </>
