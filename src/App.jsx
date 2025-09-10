64d3f0c4cf26f6145a3eacf03cc2eaba7de7c5a9
ErosChristensen <erosbarker@gmail.com>
2025-09-10 11:09:13 -1200

    Acomodo de app.jsx eros

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Noticias_Inicio from './components/Noticias_Inicio'
-import Noticias_Contenido from './components/Noticias_Contenido'
 import './App.css'
-import Footer from './components/Footer'
-import Inicio_Principal from './components/Inicio_Principal'
+//Header
 import Header from './components/Header'
-import Ultimas_Novedades from './components/Inicio_Novedades' 
+
+//Nav
+import Nav from './components/Nav'
+
+//Inicio
+import Inicio_Principal from './components/Inicio_Principal'
 import Inicio_Modalidades from './components/Inicio_Modalidades'
 import Inicio_Talleres from './components/Inicio_Talleres'
 import Inicio_Banner from './components/Inicio_Banner'  
 import Inicio_Preguntas from './components/Inicio_Preguntas'
 
+//Noticias
+import Noticias_Inicio from './components/Noticias_Inicio'
+import Noticias_Contenido from './components/Noticias_Contenido'
+import Noticias_elegida from './components/Noticias_elegida'
+import Ultimas_Novedades from './components/Inicio_Novedades' 
+
+//Footer
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-    <Header />
-    <div className=' w-full mb-20'>
-      
-      </div>
+    
+    <div className=''>
+      < Header/>
+      <Inicio_Principal />
+      <Ultimas_Novedades />
+      <Inicio_Modalidades />
+      <Inicio_Talleres />
+      <Inicio_Banner />
+      <Inicio_Preguntas />
+      <Footer />
+     </div>
 
      
     
